2e69d32263ee9a79c0ec522941f55ad74031073e
Pavel <pavel-zh@inbox.ru>
2022-05-20 12:16:13 +0300

    correct task 2

--- Lesson-2/task_2_write_order_to_json.py
+++ Lesson-2/task_2_write_order_to_json.py
@@ -7,20 +7,22 @@ def write_order_to_json(item: str,
                         price: float,
                         buyer: str,
                         date: datetime) -> 'write order to json':
-    orders_dict = {
-        "orders": [
-            {
-                "item": item,
-                "quantity": quantity,
-                "price": price,
-                "buyer": buyer,
-                "date": str(date),
-            }
-        ]
-    }
 
-    with open('orders.json', 'w', encoding='utf-8') as f:
-        json.dump(orders_dict, f, sort_keys=True, indent=4, ensure_ascii=False)
+    with open('orders.json', 'r', encoding='utf8') as f_out:
+        data = json.load(f_out)
+
+    orders = data['orders']
+    new_order_item = {
+        "item": item,
+        "quantity": quantity,
+        "price": price,
+        "buyer": buyer,
+        "date": str(date),
+    }
+    orders.append(new_order_item)
+
+    with open('orders.json', 'w', encoding='utf-8') as f_in:
+        json.dump(data, f_in, sort_keys=True, indent=4, ensure_ascii=False)
 
 
 write_order_to_json('Кресло', 5, 5200.5, 'ООО "Гиик"', datetime.now())
